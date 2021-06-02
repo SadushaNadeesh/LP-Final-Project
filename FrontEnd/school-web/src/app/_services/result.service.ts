5689aadd35b978e9d60883cdec986c9abcd26209
Sadusha Nadeesh <sadusha1994@gmail.com>
2021-06-02 21:33:25 +0530

    code review

---
+++ FrontEnd/school-web/src/app/_services/result.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpHeaders, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:3002/api/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ResultService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  get(id: any): Observable<any> {
+    return this.http.get(`${baseUrl}result/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl+'addresult', data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.patch(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByTitle(name: any): Observable<any> {
+    return this.http.get(`${baseUrl}?name=${name}`);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${baseUrl}/files`);
+  }
+}
